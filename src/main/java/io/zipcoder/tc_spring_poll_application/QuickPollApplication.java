e8369806250cd0d6272a8e00e020ff6fb56f1e36
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-09 16:28:22 -0500

    completed 3.1.3

--- src/main/java/io/zipcoder/tc_spring_poll_application/QuickPollApplication.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/QuickPollApplication.java
@@ -2,9 +2,11 @@ package io.zipcoder.tc_spring_poll_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class QuickPollApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(QuickPollApplication.class, args);
     }
